25b90e736500d17001f47744febe3a48102930c5
Christopher Dugdale <chris.dugdale3@gmail.com>
2024-08-01 05:56:11 +1000

    added user model

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app=Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///recipes.db'
@@ -8,4 +9,7 @@ app.config['SECRET_KEY']='0c70dc8668ffe8b846ace6018a789c9ef30e8458e2cbd3fd'
 
 db = SQLAlchemy(app)
 migrate = Migrate(app,db)
+
+
+login_manager= LoginManager(app)
 from myapp import routes, models
