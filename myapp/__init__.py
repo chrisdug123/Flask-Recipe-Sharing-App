8af6603f94b2cd244c3b1052a10fecd2affd3254
Christopher Dugdale <chris.dugdale3@gmail.com>
2024-08-01 04:35:06 +1000

    First model added, and made necessary command prompts to initialise and migrate model

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_migrate import Migrate
 
 app=Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='SQLITE:///recipes.db'
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///recipes.db'
+app.config['SECRET_KEY']='0c70dc8668ffe8b846ace6018a789c9ef30e8458e2cbd3fd'
 
+db = SQLAlchemy(app)
+migrate = Migrate(app,db)
 from myapp import routes, models
