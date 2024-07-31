a295c0fd0bcc50d51595d7a58d933569f82cfe49
Christopher Dugdale <chris.dugdale3@gmail.com>
2024-08-01 07:28:31 +1000

    Added registration, login and logout functionality

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -10,6 +10,12 @@ app.config['SECRET_KEY']='0c70dc8668ffe8b846ace6018a789c9ef30e8458e2cbd3fd'
 db = SQLAlchemy(app)
 migrate = Migrate(app,db)
 
+login_manager = LoginManager(app)
+from myapp.models import User
+login_manager.login_view = 'login'
 
-login_manager= LoginManager(app)
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 from myapp import routes, models
--- myapp/routes.py
+++ myapp/routes.py
@@ -1,14 +1,15 @@
 from flask import render_template, redirect, url_for
-from .models import Recipe
-from .forms import RecipeForm
+from .models import Recipe, User, check_password_hash
+from .forms import RecipeForm, RegistrationForm,LoginForm
 from . import app, db
+from flask_login import current_user, login_user, logout_user
 
 @app.route('/')
 def home():
     recipes = Recipe.query.all()
     return render_template('home.html', recipes = recipes)
 
-@app.route('/new_recipe')
+@app.route('/new_recipe', methods=['GET','POST'])
 def new_recipe():
     form = RecipeForm()
     if form.validate_on_submit():
@@ -19,3 +20,38 @@ def new_recipe():
         db.session.commit()
         return redirect(url_for('home'))
     return render_template('new_recipe.html', title="New Recipe", form=form)
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username = form.username.data, email = form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('home'))
+    
+    return render_template('register.html', title="Register", form=form)
+
+@app.route('/login', methods = ['GET','POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username = form.username.data).first()
+        if user and check_password_hash(user.password_hash, form.password.data):
+            login_user(user, remember=True)
+            return redirect(url_for('home'))
+    error="Invalid username or password"
+    return render_template('login.html', form=form, error=error)
+    
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
