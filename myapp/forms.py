0bcd81d9117f082b67ed2421f9714d22ee234ed9
Christopher Dugdale <chris.dugdale3@gmail.com>
2024-08-01 06:06:40 +1000

    create registration form and validation

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,10 +1,30 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, TextAreaField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Length
+from wtforms import StringField, IntegerField, TextAreaField, BooleanField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from .models import User
+
 
 class RecipeForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     description = TextAreaField('Description', validators=[DataRequired()])
     submit = SubmitField('Post')
 
-    
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit=SubmitField('Register')
+
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Please use a different username.')
+        
+    def validate_email(self,email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('Please user a different email address')
